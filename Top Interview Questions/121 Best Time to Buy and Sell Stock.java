fe50aa89750553f43cb23ffe9b647367406ca9bb
Aditya Gupta <addiegupta@gmail.com>
2019-06-03 14:07:26 +0530

    Add Best Time to Buy and Sell Stock

---
+++ Top Interview Questions/121 Best Time to Buy and Sell Stock.java
@@ -0,0 +1,45 @@
+/*
+
+LeetCode: 121. Best Time to Buy and Sell Stock
+
+Easy
+
+Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Acceptance: 47.3
+
+Topics: Array
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices==null || prices.length==0)return 0;
+        int ans=0,diff=0;
+        int min=prices[0];
+        for(int i=0;i<prices.length;i++){
+            diff=prices[i]-min;
+            if(diff<0)min=prices[i];
+            else if(diff>ans)ans=diff;
+        }
+        return ans;
+    }
+}
